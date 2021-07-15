357340c39a198f9e1364c15f30cf76b5b7576d03
LuizBrazJr <60703171+LuizBrazJr@users.noreply.github.com>
2021-07-15 15:45:25 -0300

    correcao pastas

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import MainGrid from './src/components/MainGrid';
-import Box from './src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from './src/lib/AlurakutCommons';
-import { ProfileRelationsBoxWrapper } from './src/components/ProfileRelations';
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedade) {
   console.log(propriedade);
@@ -34,33 +34,29 @@ export default function Home() {
           <Box>
             <h1 className="title">
               Bem-Vindos
-
-              <OrkutNostalgicIconSet />
             </h1>
+            <OrkutNostalgicIconSet />
           </Box>
         </div>
         <div className="profileRelationArea" style={{gridArea: 'profileRelationArea'}}>
-        <ProfileRelationsBoxWrapper>
-          <h2 className="smallTitle">
-            Devs da Comunidade ({pessoasFavoritas.length})
-          </h2>
-          <ul>
-            {pessoasFavoritas.map((itemAtual) => {
-              return (
-                <li>
-                  <a href={`/users/${itemAtual}`} key={itemAtual}>
-                    <img src={`https://github.com/${itemAtual}.png`} />
-                    <span>{itemAtual}</span>
-                  </a>
-                </li>            
-              )
-            })}
-          </ul>
-        </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Devs da Comunidade ({pessoasFavoritas.length})
+            </h2>
 
-        <Box>
-          Comunidades
-        </Box>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>            
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
